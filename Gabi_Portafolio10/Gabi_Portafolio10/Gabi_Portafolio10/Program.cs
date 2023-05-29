59ce23ffbd9891dc28560247bfeedbc46438f9bd
gabi672022 <mggutierrezh@hotmail.com>
2023-05-28 22:32:57 -0600

    Entrega Portafolio 10

---
+++ Gabi_Portafolio10/Gabi_Portafolio10/Gabi_Portafolio10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gabi_Portafolio10;
+
+namespace Gabi_Portafolio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TourVacaciones tour = new TourVacaciones();
+
+            tour.IngresarTour();
+            tour.MostrarTours(tour.IngresarTour());
+
+            Console.ReadKey();
+
+            var vehiculos = new[]
+            {
+                 new { Placa = "ABC123", Tipo = "Automóvil", Color = "Rojo", Año = 2020, Fabricante = "Toyota", Modelo = "Corolla" },
+                new { Placa = "DEF456", Tipo = "SUB 4x2", Color = "Gris", Año = 2019, Fabricante = "Ford", Modelo = "Escape" },
+                new { Placa = "GHI789", Tipo = "SUB 4x4", Color = "Negro", Año = 2021, Fabricante = "Jeep", Modelo = "Wrangler" },
+                new { Placa = "JKL012", Tipo = "Automóvil", Color = "Blanco", Año = 2018, Fabricante = "Honda", Modelo = "Civic" },
+                new { Placa = "MNO345", Tipo = "SUB 4x2", Color = "Azul", Año = 2022, Fabricante = "Nissan", Modelo = "X-Trail" },
+                new { Placa = "PQR678", Tipo = "Automóvil", Color = "Plata", Año = 2020, Fabricante = "Chevrolet", Modelo = "Cruze" }
+            };
+            // Ciclo foreach para mostrar los datos de los vehículos
+            Console.WriteLine("Datos de los vehículos:");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Placa\t\tTipo\t\tColor\tAño\tFabricante\tModelo");
+            Console.WriteLine("--------------------------------------------------");
+
+            foreach (var vehiculo in vehiculos)
+            {
+                Console.WriteLine($"{vehiculo.Placa}\t{vehiculo.Tipo}\t{vehiculo.Color}\t{vehiculo.Año}\t{vehiculo.Fabricante}\t{vehiculo.Modelo}");
+            }
+
+            Console.WriteLine();
+
+            //contadores
+            TourVacaciones tour1 = new TourVacaciones(1, "Playa");
+            TourVacaciones tour2 = new TourVacaciones(1, "Playa");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Gabi_Portafolio10/Gabi_Portafolio10/Gabi_Portafolio10/TourVacaciones.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Gabi_Portafolio10
+{
+    class TourVacaciones
+    {
+        static int contador = 0;
+        private int IdTour { get; set; }
+        private string Destino { get; set; }
+
+        public double precio;
+        private double Precio
+        {
+            get { return precio; }
+            set
+            {
+                if (value > 0)
+                {
+                    precio = value;
+                }
+                else
+                {
+                    Console.WriteLine("El precio debe ser mayor que cero");
+                }
+            }
+        }
+
+        private DateTime Fecha_Salida { get; set; }
+        private DateTime Fecha_Regreso { get; set; }
+        private string Descripcion { get; set; }
+
+        //método constructor con parámetros
+        public TourVacaciones(int idTour, string destino, double precio, DateTime fechaSalida, DateTime fechaRegreso, string descripcion)
+        {
+            IdTour = idTour;
+            Destino = destino;
+            Precio = precio;
+            Fecha_Salida = fechaSalida;
+            Fecha_Regreso = fechaRegreso;
+            Descripcion = descripcion;
+            contador++;
+        }
+        //método constructor sin parámetros (valores por defecto)
+        public TourVacaciones()
+        {
+            IdTour = 0;
+            Destino = "";
+            Precio = 0.0;
+            Fecha_Salida = DateTime.MinValue;
+            Fecha_Regreso = DateTime.MinValue;
+            Descripcion = "";
+            contador++;
+        }
+        public TourVacaciones(int idTour, string destino)
+        {
+            IdTour = idTour;
+            Destino = destino;
+            contador++;
+        }
+
+        //Métodos
+        public TourVacaciones [] IngresarTour ()
+        {
+            TourVacaciones[] tours = new TourVacaciones[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ingrese los datos para el tour {i+1}:");
+                Console.Write("ID del Tour: ");
+                int idTour = int.Parse(Console.ReadLine());
+
+                Console.Write("Destino: ");
+                string destino = Console.ReadLine();
+
+                Console.Write("Precio: ");
+                double precio = double.Parse(Console.ReadLine());
+
+                Console.Write("Fecha de Salida (dd/mm/aaaa): ");
+                DateTime fechaSalida = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Fecha de Regreso (dd/mm/aaaa): ");
+                DateTime fechaRegreso = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Descripción: ");
+                string descripcion = Console.ReadLine();
+
+                //Crear el objeto tourVacaciones
+                TourVacaciones tour = new TourVacaciones(idTour, destino, precio, fechaSalida, fechaRegreso, descripcion);
+
+                tours[i] = tour;
+                Console.WriteLine(); ;
+            }
+            return tours;
+        }//Fin TourVacaciones
+
+        public void MostrarTours(TourVacaciones[] tours)
+        {
+            Console.WriteLine("Datos de los tours:");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("ID\tDestino\t\tPrecio\t\tFecha Salida\tFecha Regreso\tDescripción");
+            Console.WriteLine("--------------------------------------------------");
+
+            foreach (TourVacaciones tour in tours)
+            {
+                Console.WriteLine($"{tour.IdTour}\t{tour.Destino}\t\t{tour.Precio}\t\t{tour.Fecha_Salida.ToShortDateString()}\t{tour.Fecha_Regreso.ToShortDateString()}\t{tour.Descripcion}");
+            }
+
+
+        }
+        public static void MostrarCantidadObjetos()
+        {
+            Console.WriteLine("Cantidad de objetos de la clase TourVacaciones: " + contador);
+        }
+
+
+    }//Fin class TourVacaciones
+}//Fin namespace
